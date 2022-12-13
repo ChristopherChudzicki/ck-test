071644032b901e7d01bc040e76039ab334ffb1de
Chris Chudzicki <christopher.chudzicki@gmail.com>
2022-12-13 11:23:44 -0500

    add html template, fix ckeditor build

---
+++ src/types/ckeditor.d.ts
@@ -0,0 +1,3 @@
+declare module '@ckeditor/ckeditor5-dev-webpack-plugin'
+
+declare module '@ckeditor/ckeditor5-dev-utils'
--- webpack.config.ts
+++ webpack.config.ts
@@ -1,7 +1,54 @@
 import * as path from "node:path";
 
+import CKEditorWebpackPlugin from '@ckeditor/ckeditor5-dev-webpack-plugin'
+import { styles } from '@ckeditor/ckeditor5-dev-utils'
+
 import * as w from "webpack"
 import HtmlWebpackPlugin from "html-webpack-plugin"
+
+const ckeditor = {
+  plugins: [
+    new CKEditorWebpackPlugin({
+      // See https://ckeditor.com/docs/ckeditor5/latest/features/ui-language.html
+      language: 'en'
+    })
+  ],
+  module: {
+    rules: [
+      {
+        test: /ckeditor5-[^/\\]+[/\\]theme[/\\]icons[/\\][^/\\]+\.svg$/,
+        use: ['raw-loader']
+      },
+      {
+        test: /ckeditor5-[^/\\]+[/\\]theme[/\\].+\.css$/,
+        use: [
+          {
+            loader: 'style-loader',
+            options: {
+              injectType: 'singletonStyleTag',
+              attributes: {
+                'data-cke': true
+              }
+            }
+          },
+          'css-loader',
+          {
+            loader: 'postcss-loader',
+            options: {
+              postcssOptions: styles.getPostCssConfig({
+                themeImporter: {
+                  themePath: require.resolve('@ckeditor/ckeditor5-theme-lark')
+                },
+                minify: true
+              })
+            }
+          }
+        ]
+      }
+    ]
+  }
+}
+
 
 const config: w.Configuration = {
   mode: "production",
@@ -17,13 +64,17 @@ const config: w.Configuration = {
         use: 'ts-loader',
         exclude: /node_modules/,
       },
+      ...ckeditor.module.rules,
     ],
   },
   resolve: {
     extensions: ['.tsx', '.ts', '.js'],
   },
   plugins: [
-    new HtmlWebpackPlugin(),
+    new HtmlWebpackPlugin({
+      template: 'src/index.html'
+    }),
+    ...ckeditor.plugins,
   ]
 };
 
